a501985db024050e21219d997c0e2dd65b092415
sendiliniz <longhornj84@gmail.com>
2025-06-28 03:26:09 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,61 +1,66 @@
-// server.js
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-const activeUsers = new Set();
+// Serve static files from the root directory
+app.use(express.static("."));
 
-app.use(express.static('public'));
+const activeUsers = new Map(); // Changed to Map to store user data
 
-io.on('connection', (socket) => {
-  let username = null;
+io.on("connection", (socket) => {
+  let userData = null;
 
-  socket.on('set_username', (name, callback) => {
+  socket.on("set_username", (data, callback) => {
+    const name = typeof data === 'string' ? data : data.name;
+    const avatar = typeof data === 'object' ? data.avatar : null;
+    
     if (activeUsers.has(name)) {
-      callback({ success: false, message: 'Username already taken!' });
+      callback({ success: false, message: "Username already taken!" });
     } else {
-      username = name;
-      activeUsers.add(name);
+      userData = { name, avatar };
+      activeUsers.set(name, userData);
       callback({ success: true });
     }
   });
 
-  socket.on('join_server', (serverName) => {
+  socket.on("join_server", (serverName) => {
     socket.join(serverName);
-    io.to(serverName).emit('chat_message', {
+    io.to(serverName).emit("chat_message", {
       server: serverName,
-      user: 'System',
-      message: `${username} joined #${serverName}`
+      user: "System",
+      message: `${userData?.name || 'User'} joined #${serverName}`,
     });
   });
 
-  socket.on('leave_server', (serverName) => {
+  socket.on("leave_server", (serverName) => {
     socket.leave(serverName);
-    io.to(serverName).emit('chat_message', {
+    io.to(serverName).emit("chat_message", {
       server: serverName,
-      user: 'System',
-      message: `${username} left #${serverName}`
+      user: "System",
+      message: `${userData?.name || 'User'} left #${serverName}`,
     });
   });
 
-  socket.on('send_message', ({ server, message }) => {
-    if (username && server) {
-      io.to(server).emit('chat_message', {
-        server,
-        user: username,
-        message
-      });
-    }
+  socket.on("send_message", ({ server, message, avatar }) => {
+    io.to(server).emit("chat_message", {
+      server,
+      user: userData?.name || 'Unknown',
+      message,
+      avatar: avatar || userData?.avatar,
+    });
   });
 
-  socket.on('disconnect', () => {
-    activeUsers.delete(username);
+  socket.on("disconnect", () => {
+    if (userData?.name) {
+      activeUsers.delete(userData.name);
+    }
   });
 });
 
-server.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+server.listen(5000, "0.0.0.0", () => {
+  console.log("Server running on port 5000");
 });
